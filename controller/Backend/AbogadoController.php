abdfc69f94528e019efe77938e54676334b2035f
Ascariel <pablocangas@gmail.com>
2017-07-06 22:32:15 -0400

    comenzando con graficos y estadisticas

---
+++ controller/Backend/AbogadoController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Controller\Backend;
+
+use Library\Controller;
+use Model\Entity\Usuario;
+use Model\Entity\Atencion;
+use Model\Entity\Abogado;
+
+
+class AtencionController extends Controller {
+    static $template = 'Layout/base.html.php';
+
+    function newAction(){
+
+      return [];
+    }
+
+    
+}
